68c2ba0263f14513750d1bcb0fca97e144dad6e2
rezaawp <rezaaawp@gmail.com>
2022-10-11 15:12:07 +0700

    update

---
+++ sumber/sumber.js
@@ -0,0 +1,70 @@
+const sumber = {
+	"BAB 1" : {
+		"1.1 Latar Belakang" : {
+			"referensi" : "https://docs.google.com/document/d/1EDdThdsGVl64N4GQ3Tc4B37MOXMU4OzQ/edit",
+			"src" : [
+				"https://www.smkmutucikampek.sch.id/"
+			],
+			"['Apa itu PKL ?', 'Tujuan PKL', 'Manfaat PKL', 'dll']" : [
+				"src" : [
+					"https://www.detik.com/jabar/berita/d-6266550/pkl-praktek-kerja-lapangan-tujuan-manfaat-pelaksanaan-dan-penilaian"
+				]
+			] 
+		},
+		"1.2 Maksud dan Tujuan" : {
+			"referensi" : "https://docs.google.com/document/d/1EDdThdsGVl64N4GQ3Tc4B37MOXMU4OzQ/edit",
+			"isi": ["Maksud dan Tujuan", "wajib membuat laporan praktik kerja lapangan dengan tujuan sebagai berikut"]
+			"src" : [
+				"https://www.detik.com/jabar/berita/d-6266550/pkl-praktek-kerja-lapangan-tujuan-manfaat-pelaksanaan-dan-penilaian"
+			]
+		},
+		"1.3 Manfaat PKL" : {
+			"referensi" : "https://docs.google.com/document/d/1EDdThdsGVl64N4GQ3Tc4B37MOXMU4OzQ/edit",
+			"isi" : "Manfaat PKL"
+			"src" : ""
+		}
+	},
+	"BAB 2" : {
+		"2.1 KAJIAN TEORI" : {
+			"2.1.1 PENGERTIAN PWPB" : {
+				"src" : ""
+			},
+			"2.1.2 PENGERTIAN PEMROGRAMAN BERBASIS OBJECT" : {
+				"src" : ""
+			} 
+			"2.1.3 PENGERTIAN BASIS DATA" : {
+
+			}
+		},
+		"2.2 GAMBARAN UMUM LAB RPL" : {
+			"2.2.1 Profil" : {
+				"referensi" : "https://docs.google.com/document/d/1hIlPsEBlZlbBUchpovgrBdSabwmvLutN/edit",
+				"src" : ""
+			},
+			"2.2.2 Struktur Organisasi" : {
+				"isi" : [
+					"Struktur Lab" : {
+						"Ketua Jurusan" : "Pak Aulia",
+						"Guru" : ["Pak Haris", "Pak Aul"]
+				},
+					"Struktur Sekolah" : {
+						"Letak Posisi Struktur" : "Ada di  rungan TU",
+						"Kepala Sekolah" : "Dede Setiabudi",
+						"Bendahara" : "",
+						"Staff TU" : "",
+					}
+				]
+			}
+		}
+	},
+	"BAB 3 (PELAKSANAAN KEGIATAN" : {
+		"3.1 Waktu Pelaksanan Kegiatan" : {
+			"referensi" : "https://docs.google.com/document/d/1Poc7XJ4zR4DvSJS33mXHaxoMHF0vvYsE/edit",
+			"src" : "Jurnal Kegiatan",
+		},
+		"3.2 Uraian Kegiatan" : [
+			"3.2.1 Pembuatan Website Menggunakan VS CODE",
+			"3.2.2 Pembuatan "
+		]
+	}
+}
